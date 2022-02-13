ee307bb1460a6ef2d822ae179846aa5f5db29430
B1nh M1nh <binhminhbeu@gmail.com>
2022-02-13 17:27:55 +0700

    Update state_candles.py

--- jesse/store/state_candles.py
+++ jesse/store/state_candles.py
@@ -327,11 +327,7 @@ class CandlesState:
         # no need to worry for forming candles when timeframe == 1m
         if timeframe == '1m':
             arr: DynamicNumpyArray = self.get_storage(exchange, symbol, '1m')
-            if len(arr) == 0:
-                return np.zeros((0, 6))
-            else:
-                return arr[:]
-
+            return np.zeros((0, 6)) if len(arr) == 0 else arr[:]
         # other timeframes
         dif, long_key, short_key = self.forming_estimation(exchange, symbol, timeframe)
         long_count = len(self.get_storage(exchange, symbol, timeframe))
@@ -376,11 +372,7 @@ class CandlesState:
         # no need to worry for forming candles when timeframe == 1m
         if timeframe == '1m':
             arr: DynamicNumpyArray = self.get_storage(exchange, symbol, '1m')
-            if len(arr) == 0:
-                return np.zeros((0, 6))
-            else:
-                return arr[-1]
-
+            return np.zeros((0, 6)) if len(arr) == 0 else arr[-1]
         # other timeframes
         dif, long_key, short_key = self.forming_estimation(exchange, symbol, timeframe)
         long_count = len(self.get_storage(exchange, symbol, timeframe))
@@ -390,16 +382,11 @@ class CandlesState:
         if dif != 0:
             if fullonly:
                 # remove last forming candle
-                if long_count <= 1:
-                    return np.zeros((0, 6))
-                else:
-                    return self.storage[long_key][-2]
+                return np.zeros((0, 6)) if long_count <= 1 else self.storage[long_key][-2]
             else:
                 return generate_candle_from_one_minutes(
                     timeframe, self.storage[short_key][short_count - dif:short_count],
                     True
                 )
-        if long_count == 0:
-            return np.zeros((0, 6))
-        else:
-            return self.storage[long_key][-1]
+        return np.zeros((0, 6)) if long_count == 0 else self.storage[long_key][-1]
+
